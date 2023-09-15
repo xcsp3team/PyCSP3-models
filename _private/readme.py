5f76936d5bd6c883d824d50c82b485feea3f6625
audemard <gilles.audemard@univ-artois.fr>
2023-09-15 14:56:19 +0200

    readme.py : create README.md file regarding a model in python

---
+++ _private/readme.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+if __name__ == '__main__':
+    constraints_path = "http://pycsp.org/documentation/constraints/"
+
+    model = sys.argv[1]
+    name = os.path.basename(model).split(".")[0]
+    dir = os.path.dirname(model)
+    inputlines = open(model, "r").readlines()
+    outputfile = open(f"{dir}/README.md", "w")
+    outputfile.write(f"# Problem {name}\n")
+    outputfile.write("## Description\n")
+    start = False
+    for line in inputlines:
+        if line == '"""\n':
+            if start:
+                break
+            start = True
+            continue
+        if "constraints:" in line:
+            outputfile.write("  constraints: ")
+            constraints = line.strip().split(":")[1].split(",")
+            constraints = [c.strip() for c in constraints]
+            outputfile.write(", ".join([f"[{c}]({constraints_path}{c})" for c in constraints]))
+            outputfile.write("\n")
+        else :
+            outputfile.write(line)
+    outputfile.close()
---
+++ complex-data-problems/COP/ValveNetwork/README.md
@@ -0,0 +1,24 @@
+# Problem ValveNetwork
+## Description
+This is a model for the Valve network planning for Advent of Code 2022 Day 16.
+The original model has been written by Mikael Zayenz Lagerkvist for the Minizinc challenge 2023.
+
+Instead of using different networks for varying hardness,
+this model uses different planning horizons for adjusting the hardness of the problem.
+
+## Data
+  an integer (as the network is included in the model below)
+
+## Model
+  constraints: [Sum](http://pycsp.org/documentation/constraints/Sum), [Extension](http://pycsp.org/documentation/constraints/Extension), [Intension](http://pycsp.org/documentation/constraints/Intension), [Element](http://pycsp.org/documentation/constraints/Element)
+
+## Execution
+  python ValveNetwork.py -data=6
+
+## Links
+  - https://adventofcode.com/2022
+  - https://github.com/zayenz/advent-of-code-2022
+  - https://www.minizinc.org/challenge2023/results2023.html
+
+## Tags
+  realistic, mzn23
